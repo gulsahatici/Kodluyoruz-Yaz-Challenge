0ee636af041bbc8b4a791db999e34f83db5dcce1
gulsahatici <gulsah.atici0@gmail.com>
2023-09-01 11:47:40 +0300

    Add files via upload

---
+++ 3.challenge.easy.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int isabet3 = 5;
+    int isabet2 = 10;
+
+    int puan3 = isabet3* 3; 
+    int puan2 = isabet2 * 2; 
+
+    int toplam = puan3 + puan2; 
+
+    printf("Toplam puan: %d\n", toplam);
+
+    return 0;
+}
