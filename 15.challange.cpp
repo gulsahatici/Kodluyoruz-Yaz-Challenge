bc08eda730b1b19763b199df6b6344485c4b5978
gulsahatici <gulsah.atici0@gmail.com>
2023-09-01 10:14:05 +0300

    Add files via upload

---
+++ 15.challange.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int dizi[]={10,15,34,40,12};
+	int a,b,sonuc;
+	printf("0-5 arasinda toplamak istediginiz sayinin numarasini seciniz.");
+	scanf("%d",&a);
+	printf("0-5 arasinda toplamak istediginiz diger sayinin numarasini seciniz.");
+	scanf("%d",&b);
+	sonuc=dizi[a]+dizi[b];
+	printf("Sectiginiz sayilarin toplami %d",sonuc);
+	
+	return 0;
+}
