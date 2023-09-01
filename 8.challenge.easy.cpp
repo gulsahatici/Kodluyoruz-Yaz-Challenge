d1f96efac6602f6151636d161fb838225928bd95
gulsahatici <gulsah.atici0@gmail.com>
2023-09-01 11:34:34 +0300

    Add files via upload

---
+++ 8.challenge.easy.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int sayi,sonuc;
+     
+	printf("Bir sayi giriniz : ");
+	scanf("%d",&sayi);
+	
+	sonuc=sayi*sayi;
+	printf("girdiginiz sayinin karesi: %d",sonuc);
+	
+	
+	return 0;
+}
